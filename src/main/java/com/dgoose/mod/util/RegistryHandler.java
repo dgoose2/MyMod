06419ebeee3994b1943670478d7fece739938891
dylanguzman <dylanandres.guzman@calbaptist.edu>
2020-12-20 01:22:36 -0800

    12-20-2020

--- src/main/java/com/dgoose/mod/util/RegistryHandler.java
+++ src/main/java/com/dgoose/mod/util/RegistryHandler.java
@@ -5,8 +5,9 @@ import com.dgoose.mod.blocks.BlockItemBase;
 import com.dgoose.mod.blocks.RubyBlock;
 import com.dgoose.mod.blocks.SmileBlock;
 import com.dgoose.mod.items.ItemBase;
+import com.dgoose.mod.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,24 +16,36 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
 
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Tutorial.MOD_ID);
-    public static final DeferredRegister<Item> ITEMS2 = new DeferredRegister<>(ForgeRegistries.ITEMS, Tutorial.MOD_ID);
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Tutorial.MOD_ID);
-    public static final DeferredRegister<Block> BLOCKS2 = new DeferredRegister<>(ForgeRegistries.BLOCKS, Tutorial.MOD_ID);
 
     public static void init(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ITEMS2.register(FMLJavaModLoadingContext.get().getModEventBus());
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BLOCKS2.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     // Items ONLY
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
-    public static final RegistryObject<Item> FROWN = ITEMS2.register("frown", ItemBase::new);
+    public static final RegistryObject<Item> FROWN = ITEMS.register("frown", ItemBase::new);
+
+    //Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(ModItemTier.RUBY, 0, -2.8F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(ModItemTier.RUBY, 0.5F, -3.0F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(ModItemTier.RUBY, 5, -3.1F, new Item.Properties().group(Tutorial.TAB)));
+
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(ModItemTier.RUBY, -1.0F,  new Item.Properties().group(Tutorial.TAB)));
 
     //Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
-    public static final RegistryObject<Block> SMILE_BLOCK = BLOCKS2.register("smile_block", SmileBlock::new);
+    public static final RegistryObject<Block> SMILE_BLOCK = BLOCKS.register("smile_block", SmileBlock::new);
 
     //Block Items
     public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
